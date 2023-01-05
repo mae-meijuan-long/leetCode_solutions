f4f26068a8c3c10b4b59e48b0aee571370e255a2
mae-meijuan-long <meijuan.long@miu.edu>
2023-01-04 23:01:06 -0600

    solution_19

---
+++ src/list/solution_19/ListNode.java
@@ -0,0 +1,9 @@
+package list.solution_19;
+
+ public class ListNode {
+      int val;
+      ListNode next;
+      public ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
---
+++ src/list/solution_19/Solution.java
@@ -0,0 +1,47 @@
+package list.solution_19;
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        //find it
+        ListNode target  = head;
+        ListNode nextNNode  = head;
+        for(int i = 0;i<n;i++){
+            if(nextNNode.next!=null){
+                nextNNode = nextNNode.next;
+            }else{
+                nextNNode = head;
+            }
+        }
+
+        if(nextNNode.equals(target)){
+            return target.next;
+        }
+
+        while( nextNNode.next != null){
+            nextNNode = nextNNode.next;
+            target = target.next;
+        }
+
+        var removed = target.next;
+        if(removed == null){
+            return null;
+        }
+        if( removed.next == null){
+            target.next = null;
+            return head;
+        }
+
+        if(removed != null && removed.next != null){
+            target.next = removed.next;
+        }
+
+
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        new Solution().removeNthFromEnd(listNode,2);
+    }
+}
