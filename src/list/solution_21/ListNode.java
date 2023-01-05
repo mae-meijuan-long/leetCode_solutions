2774b3f0a1b2ac7f0f730ca66c2cca7b02a2569c
mae-meijuan-long <meijuan.long@miu.edu>
2023-01-04 23:01:37 -0600

    solution_19

--- src/list/solution_21/ListNode.java
+++ src/list/solution_21/ListNode.java
@@ -3,7 +3,7 @@ package list.solution_21;
  public class ListNode {
       int val;
       ListNode next;
-      ListNode() {}
+      public ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  }
